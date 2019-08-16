50b8fe0fad46c3515992b18df40a41feb0d0138f
yavarguliyev <yavarrg@code.edu.az>
2019-08-16 12:32:47 +0400

    modal search is fixed

--- Front-End project/js/modal-search.js
+++ Front-End project/js/modal-search.js
@@ -1,9 +1,15 @@
 "use-strict";
+// Modal Search
+
 
 let modal = document.getElementById('modal');
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+$(".search-icon").click(function() {
+    $("#modal").attr("style", "display:block");
+
+})
+
+
+$(document).on("click", "#close-modal", function() {
+    $("#modal").attr("style", "display:none");
+})
